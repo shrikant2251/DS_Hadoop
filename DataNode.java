ccf6eb96295ed8552df20d916d437e68fc126279
shrikant <shrikant.2251@gmail.com>
2015-03-06 20:46:00 +0530

    Git Started NameNode and DataNode files created

---
+++ DataNode.java
@@ -0,0 +1,16 @@
+
+public class DataNode implements IDataNode{
+
+	@Override
+	public byte[] readBlock(byte[] dataBlock) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] writeBlock(byte[] dataBlock) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ INameNode.java
@@ -0,0 +1,33 @@
+public interface INameNode {
+
+	/* OpenFileResponse openFile(OpenFileRequest) */
+	/* Method to open a file given file name with read-write flag*/
+	byte[] openFile(byte fileName[]);
+	
+	/* CloseFileResponse closeFile(CloseFileRequest) */
+	byte[] closeFile(byte fileName[]);
+	
+	/* BlockLocationResponse getBlockLocations(BlockLocationRequest) */
+	/* Method to get block locations given an array of block numbers */
+	byte[] getBlockLocations(byte blockNumbers[]);
+	
+	/* AssignBlockResponse assignBlock(AssignBlockRequest) */
+	/* Method to assign a block which will return the replicated block locations */
+	byte[] assignBlock(byte blockLocations[]);
+	
+	/* ListFilesResponse list(ListFilesRequest) */
+	/* List the file names (no directories needed for current implementation */
+	byte[] list(byte fileNames[]);
+	
+	/*
+		Datanode <-> Namenode interaction methods
+	*/
+	
+	/* BlockReportResponse blockReport(BlockReportRequest) */
+	/* Get the status for blocks */
+	byte[] blockReport(byte blockStatus[]);
+	
+	/* HeartBeatResponse heartBeat(HeartBeatRequest) */
+	/* Heartbeat messages between NameNode and DataNode */
+	byte[] heartBeat(byte hearBeat[]);
+}
